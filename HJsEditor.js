50151963a5bbe2f8f8d77898ae29d06eda0a2973
vladimka <32310898+vladimka@users.noreply.github.com>
2018-03-08 16:16:49 +0300

    Add files via upload

--- HJsEditor.js
+++ HJsEditor.js
@@ -1,34 +1,52 @@
-function HJsEditor(BgColor, TextColor, Title, P, M) {
-
-	HJsEditor = this;
-	
+function HJsEditor() {
 	var D = document;
 
-	if(!BgColor){
+	this.script = function(param){
+		this.src = param.src;
 
-		D.bgColor = 'white';
+		if(!param.type){
+			this.type = 'text/javascript';
+		}else{
+			this.type = param.type;
+		}
+	};
 
-	}else{
+	this.link = function(param){
+		if(!param.rel){
+			this.rel = 'stylesheet';
+		}else{
+			this.rel = param.rel;
+		}
 
-		D.bgColor = BgColor;
 
-	}
+		this.href = param.href;
+	};
 
-	if(!Title){
-		D.title = "title";
-	}else{
-		D.title = Title;
-	}
+	this.createHR = function(param){
+		if(!param.color){
+			this.color = 'black';
+		}else{
+			this.color = param.color;
+		}
 
-	D.body.style.padding = P + 'px';
-	D.body.style.margin = M + 'px';
+		this.class = param.class;
+		this.id = param.id;
+	};
 
 	this.createText = function(param){
-
 		if(!param.type){
 			this.type = 'p';
 		}else{
 			this.type = param.type;
+			if(param.type == 'a'){
+
+				if(!param.href){
+					this.href = '#';
+				}else{
+					this.href = param.href;
+				}
+
+			}
 		}
 
 		if(!param.text){
@@ -43,14 +61,15 @@ function HJsEditor(BgColor, TextColor, Title, P, M) {
 			this.fontFamily = param.fontFamily;
 		}
 
-		if(!param.fontSize){
-			this.fontSize = 25 + 'px';
-		}else{
-			this.fontSize = param.fontSize + 'px';
-		}
+		this.fontSize = param.fontSize + 'px';
 
 		if(!param.color){
-			this.color = TextColor;
+			if(this.type == 'a'){
+				this.color == 'blue';
+			}else{
+				this.color = 'black';
+			}
+			
 		}else{
 			this.color = param.color;
 		}
@@ -67,12 +86,11 @@ function HJsEditor(BgColor, TextColor, Title, P, M) {
 			this.margin = param.margin + 'px';
 		}
 
-		this.draw();
-
-	}
+		this.class = param.class;
+		this.id = param.id;
+	};
 
 	this.createDiv = function(param){
-
 		if(!param.height){
 			this.height = 0 + 'px';
 		}else{
@@ -103,8 +121,75 @@ function HJsEditor(BgColor, TextColor, Title, P, M) {
 			this.margin = param.margin + 'px';
 		}
 
-		this.draw();
+		if(!param.borderRadius){
+			this.borderRadius = 0 + 'px';
+		}else{
+			this.borderRadius = param.borderRadius + 'px';
+		}
 
+		this.class = param.class;
+		this.id = param.id;
+	};
+
+	this.createButton = function(param){
+		if(!param.height){
+			this.height = 0 + 'px';
+		}else{
+			this.height = param.height + 'px';
+		}
+
+		if(!param.width){
+			this.width = 0 + 'px';
+		}else{
+			this.width = param.width + 'px';
+		}
+
+		if(!param.backgroundColor){
+			this.backgroundColor = 'white';
+		}else{
+			this.backgroundColor = param.backgroundColor;
+		}
+
+		if(!param.padding){
+			this.padding = 0 + 'px';
+		}else{
+			this.padding = param.padding + 'px';
+		}
+
+		if(!param.margin){
+			this.margin = 0 + 'px';
+		}else{
+			this.margin = param.margin + 'px';
+		}
+
+		if(!param.borderRadius){
+			this.borderRadius = 0 + 'px';
+		}else{
+			this.borderRadius = param.borderRadius + 'px';
+		}
+
+		if(!param.text){
+			this.text = 'text';
+		}else{
+			this.text = param.text;
+		}
+
+		this.class = param.class;
+		this.id = param.id;
+	};
+
+	this.createHR.prototype= {
+		draw : function(){
+
+			var hr = D.createElement("hr");
+
+			hr.style.color = this.color;
+			hr.class = this.class;
+			hr.id = this.id;
+
+			D.body.appendChild(hr);
+
+		}
 	};
 
 	this.createText.prototype = {
@@ -117,15 +202,16 @@ function HJsEditor(BgColor, TextColor, Title, P, M) {
 			text.style.fontSize = this.fontSize;
 			text.style.fontFamily = this.fontFamily;
 			text.style.color = this.color;
+			text.href = this.href;
+			text.class = this.class;
+			text.id = this.id;
 
 			D.body.appendChild(text);
 
 		}
-
 	};
 
 	this.createDiv.prototype = {
-
 		draw : function(){
 
 			var div = D.createElement("div");
@@ -135,11 +221,54 @@ function HJsEditor(BgColor, TextColor, Title, P, M) {
 			div.style.backgroundColor = this.backgroundColor;
 			div.style.padding = this.padding;
 			div.style.margin = this.margin;
+			div.style.borderRadius = this.borderRadius;
+			div.class = this.class;
+			div.id = this.id;
+			
 
 			D.body.appendChild(div);
 
-		},
-
+		}
 	};
 
-}
+	this.createButton.prototype = {
+		draw : function(){
+
+			var btn = D.createElement("button");
+
+			btn.style.width = this.width;
+			btn.style.height = this.height;
+			btn.style.backgroundColor = this.backgroundColor;
+			btn.style.padding = this.padding;
+			btn.style.margin = this.margin;
+			btn.innerText = this.text;
+			btn.class = this.class;
+			btn.id = this.id;
+
+			D.body.appendChild(btn);
+
+		},
+	};
+
+	this.link.prototype = {
+		createLink : function(){
+			var link = D.createElement("link");
+
+			link.rel = this.rel;
+			link.href = this.href;
+
+			D.head.appendChild(link);
+		}
+	};
+
+	this.script.prototype = {
+		createScript : function(){
+			var script = D.createElement("script");
+
+			script.type = this.type;
+			script.src = this.src;
+
+			D.head.appendChild(script);
+		}
+	};
+};
